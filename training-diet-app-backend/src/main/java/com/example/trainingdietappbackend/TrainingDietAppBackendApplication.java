2c42f4b51b85533ee27d2bc85c1f1a8a0d18cd3f
s22288 <forgithubProject1188@gmail.com>
2023-05-28 22:15:46 +0200

    init push

---
+++ training-diet-app-backend/src/main/java/com/example/trainingdietappbackend/TrainingDietAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingdietappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingDietAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingDietAppBackendApplication.class, args);
+	}
+
+}
---
+++ training-diet-app-backend/src/test/java/com/example/trainingdietappbackend/TrainingDietAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingdietappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingDietAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
